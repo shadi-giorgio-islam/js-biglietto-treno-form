e6e093d0febf5903877b71ecfa69a8749db279a5
Giorgio <shadigiorgioislam@gmail.com>
2020-12-15 17:17:13 +0100

    Add hidden effect and END

--- js/script.js
+++ js/script.js
@@ -4,15 +4,10 @@ function() {
   var nome = document.getElementById('nome').value;
   var distanza = document.getElementById('km').value;
   var eta = document.getElementById('eta').value;
-  console.log('eta');
+  console.log(eta);
   var prezzo = 0.21 * distanza;
   var offerta = 'Tariffa standard';
 
-  document.getElementById('nome-passeggero').innerHTML = nome;
-  document.getElementById('sconto').innerHTML = offerta;
-  document.getElementById('carrozza').innerHTML = Math.floor(Math.random()*9) + 1;
-  document.getElementById('cp').innerHTML = Math.floor(Math.random()*9999) + 90000;
-  document.getElementById('prezzo').innerHTML = prezzo.toFixed(2) + ' €';
 
   if (eta == 'minorenne'){
     prezzo -= prezzo * 0.2;
@@ -22,5 +17,22 @@ function() {
     prezzo -= prezzo * 0.4;
     offerta = 'Tariffa over 65';
   }
+
+  document.getElementById("biglietto").className = "show";
+  document.getElementById('nome-passeggero').innerHTML = nome;
+  document.getElementById('sconto').innerHTML = offerta;
+  document.getElementById('carrozza').innerHTML = Math.floor(Math.random()*9) + 1;
+  document.getElementById('cp').innerHTML = Math.floor(Math.random()*9999) + 90000;
+  document.getElementById('prezzo').innerHTML = prezzo.toFixed(2) + ' €';
+}
+);
+
+var annulla = document.getElementById("annulla");
+annulla.addEventListener('click',
+function() {
+  document.getElementById('nome').value = '';
+  document.getElementById('km').value = '';
+  document.getElementById('eta').value = '';
+  document.getElementById("biglietto").className = "hidden";
 }
 );
